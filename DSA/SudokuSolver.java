e8e8c23d544316fd2621d735d0dbfdd628dccb7d
Shrasht <shrashtsingh@gmail.com>
2024-10-13 00:03:08 +0530

    Added new Programs

---
+++ DSA/SudokuSolver.java
@@ -0,0 +1,60 @@
+public class SudokuSolver {
+    private static final int N = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solve(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    public static boolean solve(int[][] board) {
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < N; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= N; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int x = 0; x < N; x++) {
+            if (board[row][x] == num || board[x][col] == num || 
+                board[row - row % 3 + x / 3][col - col % 3 + x % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int r = 0; r < N; r++) {
+            for (int d = 0; d < N; d++) {
+                System.out.print(board[r][d] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
