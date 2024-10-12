77ef3c2f224ec749d6e365867e9e4a2c5994cdc7
RiyaHoro <145681087+RiyaHoro@users.noreply.github.com>
2024-10-12 17:47:13 +0530

    Create missing_number.java
    Find a missing number.

---
+++ DSA/missing_number.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.BitSet;
+ 
+/**
+ * Java program to find missing elements in a Integer array containing 
+ * numbers from 1 to 100.
+ *
+ * @author Javin Paul
+ */
+public class MissingNumberInArray {
+ 
+    public static void main(String args[]) {
+
+        // one missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6}, 6);
+ 
+        // two missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6, 7, 9, 8, 10}, 10);
+ 
+        // three missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 6, 9, 8}, 10);
+ 
+        // four missing number
+        printMissingNumber(new int[]{1, 2, 3, 4, 9, 8}, 10);
+ 
+        // Only one missing number in array
+        int[] iArray = new int[]{1, 2, 3, 5};
+        int missing = getMissingNumber(iArray, 5);
+        System.out.printf("Missing number in array %s is %d %n", 
+                           Arrays.toString(iArray), missing);
+    }
+   /**
+    * A general method to find missing values from an integer array in Java.
+    * This method will work even if array has more than one missing element.
+    */
+    private static void printMissingNumber(int[] numbers, int count) {
+        int missingCount = count - numbers.length;
+        BitSet bitSet = new BitSet(count);
+ 
+        for (int number : numbers) {
+            bitSet.set(number - 1);
+        }
+ 
+        System.out.printf("Missing numbers in integer array %s, with total number %d is %n",
+        Arrays.toString(numbers), count);
+        int lastMissingIndex = 0;
+
+        for (int i = 0; i < missingCount; i++) {
+            lastMissingIndex = bitSet.nextClearBit(lastMissingIndex);
+            System.out.println(++lastMissingIndex);
+        }
+ 
+    }
+
+
