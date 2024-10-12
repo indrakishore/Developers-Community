8086580cb1687a1b807e63f572b625b75b93cbe1
Krishna Yadav <143788465+krishna1584@users.noreply.github.com>
2024-10-13 00:36:20 +0530

    Update MergekSortedListsByKrishna.java

--- DSA/MergekSortedListsByKrishna.java
+++ DSA/MergekSortedListsByKrishna.java
@@ -1,3 +1,27 @@
+/* 
+Problem Statement:
+Given an array of k sorted linked lists, merge them into one sorted linked list. If the input list is empty or contains only empty lists, return null.
+
+Example:
+Input:
+lists = [[1->4->5], [1->3->4], [2->6]]
+Output:
+[1->1->2->3->4->4->5->6]
+
+Approach:
+Divide and Conquer: Recursively divide the array of linked lists into two halves and merge them.
+Merge Two Lists: Use a helper function to merge two sorted linked lists by comparing node values.
+Time Complexity: O(n log k), where n is the total number of nodes and k is the number of linked lists.
+Space Complexity: O(log k) due to recursion depth.
+Steps:
+If lists is empty, return null.
+Recursively merge linked lists using the helper mergeKListsHelper.
+Use the merge function to merge two lists by comparing nodes one by one.
+This approach efficiently merges k lists with minimal time complexity.
+*/
+
+
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if (lists == null || lists.length == 0) {
