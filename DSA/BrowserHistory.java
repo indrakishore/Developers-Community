5bc39a23112fd0b59f34d485d5c9658afe46df46
Krushit-Babariya <148842311+Krushit-Babariya@users.noreply.github.com>
2024-10-20 16:41:23 +0530

    Update BrowserHistory.java

--- DSA/BrowserHistory.java
+++ DSA/BrowserHistory.java
@@ -1,5 +1,29 @@
 import java.util.Stack;
 
+/**
+ * Problem Statement:
+ * Design a simplified browser history system that allows a user to:
+ * - Visit a URL and update the current page.
+ * - Go back a specified number of steps in the browser history.
+ * - Go forward a specified number of steps in the browser history.
+ * 
+ * The system maintain two stacks: one for the back history and one for the forward history,
+ * mimicking how a typical browser works. When navigating back, the current page should be moved 
+ * to the forward stack, and when navigating forward, the current page should be moved back to the 
+ * back stack.
+ *
+ * Approach:
+ * - Use two stacks: backStack and forwardStack.
+ * - The backStack holds pages that the user has previously visited.
+ * - The forwardStack holds pages the user has navigated back from.
+ * - When visiting a new URL, push the current page to the backStack and clear the forwardStack 
+ *   (since the forward history is reset on a new visit).
+ * - The `back` method pops pages from the backStack and pushes them to the forwardStack until 
+ *   either the number of steps is reached or there are no more pages to go back.
+ * - The `forward` method pops pages from the forwardStack and pushes them to the backStack until 
+ *   either the number of steps is reached or there are no more pages to go forward.
+ */
+
 class BrowserHistory {
     private Stack<String> backStack;
     private Stack<String> forwardStack;
